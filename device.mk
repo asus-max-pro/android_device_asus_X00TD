aebf649c6b37899cff721068022ebcda3a0fc0b4
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-11-28 19:48:36 -0500

    X00TD: Build new GNSS HAL
    * Remove unnecessary dependencies on liblbs_core.so
    * Hardcode vendor loc HIDL version to 3.0 to enable vendor HIDL when
      building w/o BSP.
    
    Change-Id: I7ae6060d25b5b94f8e0eed86f208fc4eeba76c70
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -190,18 +190,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
-# GPS / Location
+# GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    libcurl \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti \
+    libbatching \
+    libgeofencing \
     libgnss \
-    libgnsspps \
     libgps.utils \
     liblocation_api \
-    libloc_core \
-    libloc_pla \
     libsensorndkbridge \
-    libvehiclenetwork-native
+    libwifi-hal-ctrl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
@@ -399,7 +398,7 @@ PRODUCT_BOOT_JARS += \
 # QCOM
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect
-    
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
